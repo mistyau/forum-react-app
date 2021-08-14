a0c4b3aedeaa33e20b2a726e17079d84d18f05b9
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-14 07:04:18 -0400

    Make some routes restricted to signed in users only. Refactor later, or possibly never.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
 import Login from './components/Login';
+import Home from './components/Home';
 import useToken from './components/userToken';
+import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
+import SignUp from './components/Signup';
 
 function App() {
   const { token, setToken } = useToken();
 
-  if (!token) {
-    return <Login setToken={setToken} />;
-  }
-
   return (
     <div className="wrapper">
-      <h1>Application</h1>
       <BrowserRouter>
         <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/signup">
+            {token ? <Redirect to="/dashboard" /> : <SignUp/>}
+          </Route>
+          <Route path="/login">
+            {token ? <Redirect to="/dashboard" /> : <Login setToken={setToken} />}
+          </Route>
           <Route path="/dashboard">
-            <Dashboard />
+            {token ? <Dashboard/> : <Redirect to="/login" />}
           </Route>
           <Route path="/preferences">
             <Preferences />
