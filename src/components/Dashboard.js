40fe940525c9bec38cb638e166bd3fc1819aff23
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-19 01:56:31 -0400

    Add Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
+import CreateThread from "./CreateThreadForm";
+import UserThreadList from "./UserThreadList";
 
-export default function Dashboard() {
+export default function Dashboard({user}) {
 
   return(
     <div className="dashboard-wrapper">
       <h2>Dashboard</h2>
+      <h3>Welcome, {user.username}</h3>
+      <CreateThread user={user}/>
+      <UserThreadList user={user}/>
     </div>
   );
 }
