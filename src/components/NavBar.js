d6e3996bf2551ca0cf87f6ccef72129b04e9843e
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-19 01:49:30 -0400

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+
+export default function NavBar(){
+
+    return (
+        <div className="topnav">
+            <a className="active" href="/">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    );
+}
