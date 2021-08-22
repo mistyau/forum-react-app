36f9137eba01b9285cd83b4d58d26e826d727e87
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-21 23:44:39 -0400

    Bootstrap NavBar component. Render conditionally based on login status.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,55 @@
+import Button from "react-bootstrap/Button"
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-export default function NavBar(){
+export default function NavBar({ user }){
+    const history = useHistory();
+    const [loggedIn, setLoggedIn] = useState(user.token ? true : false);
+
+    const handleClick = () => {
+        history.push("/login");
+    }
+
+    const logout = () => {
+        sessionStorage.clear();
+        history.push("/");
+        setLoggedIn(false);
+    }
+
+    useEffect(() => {
+        setLoggedIn(user.token ? true : false);
+    }, [user.token]);
+
+    if (loggedIn) {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">Forum</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav>
+                    <Navbar.Text>
+                        Signed in as: <a href="/dashboard">{user.username}</a>
+                    </Navbar.Text>
+                    <Button variant="outline-success" onClick={logout}>Log Out</Button>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 
     return (
-        <div className="topnav">
-            <a className="active" href="/">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Forum</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav>
+                <Button variant="outline-success" onClick={handleClick}>Login</Button>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
