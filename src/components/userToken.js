47a2edd3a86f107aeeea3f2a2f2ad6c6918df383
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-31 19:58:00 -0400

    Set refresh token at login

--- src/components/userToken.js
+++ src/components/userToken.js
@@ -7,18 +7,21 @@ export default function useToken() {
     const getToken = () => {
         const tokenString = sessionStorage.getItem('token');
         const usernameString = sessionStorage.getItem('username');
+        const refreshString = sessionStorage.getItem('refresh_token');
 
-        const user = { token:       JSON.parse(tokenString),
-                       username:    JSON.parse(usernameString)};
+        const user = { token:           JSON.parse(tokenString),
+                       username:        JSON.parse(usernameString),
+                       refresh_token:   JSON.parse(refreshString)};
         return user;
     };
 
     const [token, setToken] = useState(getToken());
 
     const saveToken = userToken => {
-        sessionStorage.setItem('token', JSON.stringify(userToken.token));
+        sessionStorage.setItem('token', JSON.stringify(userToken.access_token));
         sessionStorage.setItem('username', JSON.stringify(userToken.username));
-        setToken({ token: userToken.token, username: userToken.username }); // should only set state once
+        sessionStorage.setItem('refresh_token', JSON.stringify(userToken.refresh_token));
+        setToken({ token: userToken.access_token, username: userToken.username, refresh_token: userToken.refresh_token }); // should only set state once
     };
 
     return {
