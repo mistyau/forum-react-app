32b24cd01a6b7a774cb0efae9e896b923eed9f12
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-31 20:06:19 -0400

    Create EditThreadModal component

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -3,6 +3,42 @@ import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
 import Modal from "react-bootstrap/Modal";
 
+export function EditThreadModal({ show, handleClose, setSubject, setContent, handleEdit, id, subject, content }) {
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit post</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className="mb-2">
+                        <FormControl
+                            aria-label="Thread Subject"
+                            onChange={e => setSubject(e.target.value)}
+                            value={subject || ''} />
+                    </InputGroup>
+                    <InputGroup>
+                        <FormControl 
+                            as="textarea" 
+                            aria-label="With textarea"
+                            onChange={e => setContent(e.target.value)}
+                            value={content || ''} />
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleEdit(id)}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
 export default function EditPostModal({ show, handleClose, setContent, handleEdit, id, content }) {
 
     return (
