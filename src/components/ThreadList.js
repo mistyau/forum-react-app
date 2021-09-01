8ad0be9cfe1ac1a3517d723e4061ba7602e2e692
mistyau <54221165+mistyau@users.noreply.github.com>
2021-08-31 20:28:36 -0400

    Use custom instance of axios

--- src/components/ThreadList.js
+++ src/components/ThreadList.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { instance } from "../services";
 
 const baseURL = 'http://localhost:8080/api/v1';
 
 function Thread({ thread }) {
     return (
-        <Container fluid className="thread-wrapper">
+        <div className="thread-wrapper">
             <Link to={"/thread/" + thread.id}>
                 {thread.subject}
             </Link>
             <p>by {thread.author} at { thread.createdAt }</p>
-        </Container>
+        </div>
     );
 };
 
@@ -22,7 +23,7 @@ export default function ThreadList() {
     const [threads, setThreads] = useState(null);
 
     useEffect(() => {
-        axios.get(baseURL + '/threads')
+        instance.get('/threads')
             .then((response) => {
                 setThreads(response.data);
             })
@@ -36,7 +37,7 @@ export default function ThreadList() {
     }
 
     return (
-        <Container fluid className="threads-container">
+        <Container fluid className="thread-container">
             {threads.map((currentThread) => (
                 <Thread thread={currentThread} key={currentThread.id}/>
             ))}
