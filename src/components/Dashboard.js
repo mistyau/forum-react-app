af64d6c74b43528056c792192860d23e1bc82a30
mistyau <54221165+mistyau@users.noreply.github.com>
2021-09-10 03:26:39 -0400

    Use Nav component instead of Tabs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,40 @@
 import CreateThread from "./CreateThreadForm";
-import PostThreadList from './UserPostList';
 import UserThreadList from "./UserThreadList";
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
+import { useState } from "react";
+import { Nav } from "react-bootstrap";
+import { Route, Switch, useRouteMatch } from "react-router";
+import UserPostList from "./UserPostList";
 
 export default function Dashboard({user}) {
+  let { path, url } = useRouteMatch();
 
-  return(
+  return (
     <Container fluid>
       <Container fluid className="thread-wrapper">
-        <Tabs defaultActiveKey="threads" id="uncontrolled-tab-example" className="mb-3">
-          <Tab eventKey="home" title="Activity">
-            <CreateThread user={user} />
-          </Tab>
-          <Tab eventKey="threads" title="Threads">
+        <Nav className="mb-3">
+          <Nav.Item>
+            <Nav.Link href={`${url}`}>Activity</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href={`${url}/threads`}>Threads</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href={`${url}/posts`}>Posts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Switch>
+          <Route exact path={path}>
+            Under Construction
+          </Route>
+          <Route path={`${path}/threads`}>
             <UserThreadList user={user} />
-          </Tab>
-          <Tab eventKey="posts" title="Posts">
-            <PostThreadList user={user} />
-          </Tab>
-        </Tabs>
-        {/*<CreateThread user={user}/>
-        <UserThreadList user={user}/>*/}
+          </Route>
+          <Route path={`${path}/posts`}>
+            <UserPostList user={user} />
+          </Route>
+        </Switch>        
       </Container>
     </Container>
   );
