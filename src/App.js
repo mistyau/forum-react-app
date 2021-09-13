66755bce562fa55b6ecfbf9082d1b69e57955380
mistyau <54221165+mistyau@users.noreply.github.com>
2021-09-12 22:03:13 -0400

    Use sass instead of css. Add custom sass file to customize bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
