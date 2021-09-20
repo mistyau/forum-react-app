cad953089d987413eb6956a052e34a329746bb5e
mistyau <54221165+mistyau@users.noreply.github.com>
2021-09-20 06:36:09 -0400

    Add SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,29 @@
+import Nav from 'react-bootstrap/Nav';
+import { useHistory, useRouteMatch } from 'react-router';
+
+export default function SideBar({ user }) {
+    const { url } = useRouteMatch();
+    const history = useHistory();
+
+    const handleSelect = (selectedKey) => {
+        history.push(selectedKey);
+    }
+
+    return (
+        <div className="sidebar">
+            <Nav 
+                variant="pills" 
+                defaultActiveKey={`${url}`}
+                onSelect={(selectedKey) => {handleSelect(selectedKey)}}
+                className="flex-sm-column"
+            >
+                <Nav.Link eventKey={`${url}`}>Home</Nav.Link>
+                <Nav.Link eventKey={`${url}/threads`}>My Threads</Nav.Link>
+                <Nav.Link eventKey={`${url}/posts`}>My Posts</Nav.Link>
+                <Nav.Link eventKey="disabled" disabled>
+                    Liked Threads
+                </Nav.Link>
+            </Nav>
+        </div>
+    );
+}
