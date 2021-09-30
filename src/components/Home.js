d05e664ef0f29e410044d85e6e7fe238c9482441
mistyau <54221165+mistyau@users.noreply.github.com>
2021-09-29 20:23:12 -0400

    Update Home component. Add TopTagsList component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
 import ThreadList from "./ThreadList";
+import TopTagsList from "./TopTagsList";
+import Col from "react-bootstrap/Col";
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
 
 export default function Home({ user }) {
 
     return (
-        <div className="homepage">
-            <ThreadList user={user} />
-        </div>
+        <Container fluid className="homepage">
+            <Row>
+                <Col xl={9}>
+                    <ThreadList user={user} />
+                </Col>
+                <Col xl={3} className="d-none d-md-block">
+                    <Container fluid className="right-button">
+                        <TopTagsList />
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
         
     );
 };
