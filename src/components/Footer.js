1def2a56ae239092e29689a1b1961f0add218b60
mistyau <54221165+mistyau@users.noreply.github.com>
2021-10-03 08:41:20 -0400

    Add footer information. Update css.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 export default function Footer() {
 
     return (
         <footer className="footer">
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <span className="text-muted">&copy; 2021</span>
+                <AiFillGithub className="github-icon" onClick={() => window.open('https://github.com/mistyau', '_blank').focus()} />
+                <AiFillLinkedin className="linkedin-icon" onClick={() => alert("i don't have a linkedin")} />
             </div>
         </footer>
     );
